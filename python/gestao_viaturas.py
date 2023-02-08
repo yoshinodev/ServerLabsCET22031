5cdffd448df879a60f71ff15bbd29984449adcac
jgalamba <jfgalamba@mail>
2023-02-08 16:59:22 +0000

    Added files for the exercise "Gestão de Viaturas"

---
+++ python/gestao_viaturas.py
@@ -0,0 +1,15 @@
+# # matricula (str), marca, modelo, data
+# 10-XY-20,Opel,Corsa XL,2019-10-15
+# 20-PQ-15,Mercedes,300SL,2017-05-31
+
+# Menu
+#       1 - Listar Viaturas
+#       2 - Pesquisar Viaturas
+#       3 - Adicionar Viatura
+#       4 - Remover Viatura
+#       5 - Actualizar Catálogo
+#       6 - Recarregar Catálogo
+#       T - Terminar
+# 
+#       Opção >> 
+
